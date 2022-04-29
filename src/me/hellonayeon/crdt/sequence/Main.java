4f565d0215c875ecfaabd04f10f9b7c6218683d5
hellonayeon <hellonykwon@gmail.com>
2022-04-29 18:04:10 +0900

    프로젝트 생성

---
+++ src/me/hellonayeon/crdt/sequence/Main.java
@@ -0,0 +1,12 @@
+package me.hellonayeon.crdt.sequence;
+
+public class Main {
+
+    static String hello() {
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+	    // write your code here
+    }
+}
---
+++ test/me/hellonayeon/crdt/sequence/MainTest.java
@@ -0,0 +1,12 @@
+package me.hellonayeon.crdt.sequence;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void testTest() throws Exception {
+        Assertions.assertEquals(Main.hello(), "hello");
+    }
+}
